0e47a03763dc52d3d8311bcd5536931513e607a9
jodeyy <jodiptra@gmail.com>
2024-10-11 16:44:43 +0700

    commit latihan evc

---
+++ express-mvc/app_server/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const mainController = require('../controllers/main');
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+// });
+
+router.get('/', mainController.index);
+//buat route ke halaman kontak (/kontak)dengan method get
+router.get("/kontak", mainController.kontak);
+module.exports = router;
+
